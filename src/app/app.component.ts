ddae1237a06eda1d47f568bab4d52f46a02e0778
Bhushan Madaye <bhushan.madaye@gmail.com>
2022-10-22 01:15:44 +0530

    temp ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-sessions';
+
+  userValue: string;
+  @ViewChild('paraRef') paraRef: ElementRef;
+
+  addText(element) {
+    console.log(element.value)
+    this.userValue = element.value
+  }
+
+  changeColor() {
+    console.log(this.paraRef)
+    this.paraRef.nativeElement.style.color = 'red';
+  }
+
+
+
+  
+
+
+
+
+
+
+  
+  getElement(element) {
+    console.log(element.height);
+    element.src = 'assets/angular.png';
+  }
 }
